88dff12db2ed66a97c3ba02dd0cae69188b62dd9
abarojohncleo <abarojohncleo@gmail.com>
2022-02-20 10:23:33 +0800

    headers

--- routes/api.php
+++ routes/api.php
@@ -1,4 +1,7 @@
 <?php
+header('Access-Control-Allow-Origin:  *');
+header('Access-Control-Allow-Methods:  POST, GET, OPTIONS, PUT, PATCH, DELETE');
+header('Access-Control-Allow-Headers: Accept, Content-Type, X-Auth-Token, Origin, Authorization');
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ProductController;
